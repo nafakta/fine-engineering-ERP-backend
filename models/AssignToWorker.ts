7a8a6543a2476cbc1317e2b37ab849a67e5f51e0
Danish Khan <danish@dynsimulation.com>
2026-03-03 01:22:24 +0530

    done

--- models/AssignToWorker.ts
+++ models/AssignToWorker.ts
@@ -115,6 +115,7 @@ export class AssignToWorker
               "completed",
               "not-ok",
               "rejected",
+              "machine",  // Add this line
             ]],
           },
         },
